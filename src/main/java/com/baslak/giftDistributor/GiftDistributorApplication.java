c6fdf8b49ee6798a937fba5e109c38e490cc901b
Vadim Baslak <VadimBaslak@Darias-MacBook-Pro.local>
2019-08-19 22:07:51 +0700

    Initial commit

---
+++ src/main/java/com/baslak/giftDistributor/GiftDistributorApplication.java
@@ -0,0 +1,13 @@
+package com.baslak.giftDistributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftDistributorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftDistributorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baslak/giftDistributor/GiftDistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baslak.giftDistributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftDistributorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
